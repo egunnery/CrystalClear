5d7f5dfdbaadf5d945cd6c1a6c8b03c720773c87
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-11-13 14:53:48 +0000

    Pop up connected to buttons on lifestyle tips tab

--- CrystalClear/FirstViewController.swift
+++ CrystalClear/FirstViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import NMPopUpViewSwift
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var showPopupBtn: UIButton!
+    
+    var popViewController : PopUpViewControllerSwift!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func showPopUp(_ sender: AnyObject) {
+        let bundle = Bundle(for: PopUpViewControllerSwift.self)
+        if (UIDevice.current.userInterfaceIdiom == .pad)
+        {
+              self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController_iPad", bundle: bundle)
+                      self.popViewController.title = "This is a popup view"
+                      self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+        } else
+        {
+            if UIScreen.main.bounds.size.width > 320 {
+                if UIScreen.main.scale == 3 {
+                    self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController_iPhone6Plus", bundle: bundle)
+                    self.popViewController.title = "This is a popup view"
+                    self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+                } else {
+                    self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController_iPhone6", bundle: bundle)
+                    self.popViewController.title = "This is a popup view"
+                    self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+                }
+            } else {
+                self.popViewController = PopUpViewControllerSwift(nibName: "PopUpViewController", bundle: bundle)
+                self.popViewController.title = "This is a popup view"
+                self.popViewController.showInView(self.view, withImage: UIImage(named: "typpzDemo"), withMessage: "You just triggered a great popup window", animated: true)
+            }
+
+        }
+    }
+    
+    func setRoundedBorder(_ radius : CGFloat, withBorderWidth borderWidth: CGFloat, withColor color : UIColor, forButton button : UIButton)
+    {
+        let l : CALayer = button.layer
+        l.masksToBounds = true
+        l.cornerRadius = radius
+        l.borderWidth = borderWidth
+        l.borderColor = color.cgColor
+    }
 
 }
 
