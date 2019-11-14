a9b52a6445dd407a53b7985e882ae97bf0950033
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-11-14 11:35:02 +0000

    Pop up using swift code rather than a pod

--- CrystalClear/FirstViewController.swift
+++ CrystalClear/FirstViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBAction func showAlertButtonTapped(_ sender: UIButton) {
+
+    // create the alert
+    let alert = UIAlertController(title: "Eat Mindfully", message: "Eat mindfully. In our fast-paced world, there can be a tendency to eat while distraced and shovel in more food than we need. Try eating more slowly and taking time to taste food properly. Eat mindfully. In our fast-paced world, there can be a tendency to eat while distraced and shovel in more food than we need. Try eating more slowly and taking time to taste food properly.Eat mindfully. In our fast-paced world, there can be a tendency to eat while distraced and shovel in more food than we need. Try eating more slowly and taking time to taste food properly. Eat mindfully. In our fast-paced world, there can be a tendency to eat while distraced and shovel in more food than we need. Try eating more slowly and taking time to taste food properly", preferredStyle: UIAlertController.Style.alert)
+
+    // add an action (button)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+    // show the alert
+    self.present(alert, animated: true, completion: nil)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
 
 }
 
